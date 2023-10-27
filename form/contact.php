8fecca667bb3147c0fbfd826c1a9136f4cd470de
gonzalohal <87822427+gonzalohal@users.noreply.github.com>
2023-10-26 22:33:38 -0300

    Update contact.php

--- form/contact.php
+++ form/contact.php
@@ -4,10 +4,10 @@
  */
 
 // an email address that will be in the From field of the email.
-$from = 'Demo contact form <gonzalohal@gmail.com>';
+$from = 'gonzalohal@gmail.com';
 
 // an email address that will receive the email with the output of the form
-$sendTo = 'Demo contact form <gonzalohal@gmail.com>';
+$sendTo = 'gonzalohal@gmail.com';
 
 // subject of the email
 $subject = 'New message from contact form';
